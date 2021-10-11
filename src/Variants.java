3b2d71aa9b4a98297450ce4605cfa40a423a4a3c
Kirillgutsalo <kirya.sever11@gmail.com>
2021-10-11 12:55:39 +0300

    adds variant 1

--- src/Variants.java
+++ src/Variants.java
@@ -2,7 +2,8 @@
 public class Variants {
 
 	public static void main(String[] args) {
-		System.out.println("There are following variants: ");
+		System.out.println("There are following variants:");
+		System.out.println("\n- Variant 1");
 
 	}
 
